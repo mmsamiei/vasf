8c57b80c098cbfbbef716ddc535e9ffa7fdde106
mmsamiei <mohmahsamiei@gmail.com>
2022-09-26 14:51:38 +0330

    add utils.imsshow

--- utils/utils.py
+++ utils/utils.py
@@ -8,3 +8,15 @@ def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     plt.imshow(np.transpose(img, (1, 2, 0)))
     plt.show()
+
+
+def imsshow(imgs, fig_size=(6,4)):
+    num_images = imgs.shape[0]
+    fig, ax = plt.subplots(1,num_images)
+    fig.set_size_inches(fig_size)
+    fig.set_dpi(100)
+    for i, img in enumerate(imgs):
+        img = img / 2 + 0.5     # unnormalize
+        npimg = img.numpy()
+        ax[i].imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
