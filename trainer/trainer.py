b4117edec187b91f31d293cd444fd743db5f5113
mmsamiei <mohmahsamiei@gmail.com>
2022-09-27 23:01:04 +0330

    add vis validation

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,6 +1,7 @@
 from trainer.base_trainer import BaseTrainer
 import torch.nn as nn
 from tqdm.auto import tqdm
+from utils.utils import imsshow
 class SimpleTrainer(BaseTrainer):
     def __init__(self, train_dl, valid_dl, vasf_model, optimizer, dev):
         super().__init__()
@@ -12,17 +13,27 @@ class SimpleTrainer(BaseTrainer):
         self.dev = dev
         self.model = self.model.to(dev)
 
-    def train(self, num_iter, log_every_step=100):
+    def train(self, num_iter, log_every_step, dsc_len):
         for i, batch in tqdm(enumerate(self.train_dl)):
             images = batch['images'].float().to(self.dev)
             self.optimizer.zero_grad()
-            vasf_result = self.model.reconstruct(images, 2)
+            vasf_result = self.model.reconstruct(images, dsc_len)
             commit_loss = vasf_result['commit_loss'].mean()
             reconstructed = vasf_result['output']
-            loss = self.criterion(reconstructed, images) + commit_loss
+            loss = self.criterion(reconstructed, images) + 0*commit_loss
             loss.backward()
             self.optimizer.step()
             if (i+1) % log_every_step == 0:
                 print(loss.item())
+                self.visualization_validation()
             if (i+1) == num_iter:
                 break
+    
+
+    def visualization_validation(self):
+        images = next(iter(self.valid_dl))['images'].float().to(self.dev)
+        imsshow(images[:4].cpu())
+        vasf_result = self.model.reconstruct(images, 2)
+        reconstructed = vasf_result['output']
+        imsshow(reconstructed[:4].detach().cpu())
+
